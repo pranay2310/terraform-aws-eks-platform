97864e1e89c49ec3f950a23cc1827e0cb819256b
Pranaysagar <pranaysagarbhelave@gmail.com>
2026-08-03 22:52:42 +0530

    restructured

--- environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "pranay-dev-terraform-state-001"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-aws-eks-platform-dev-tf-lock"
-    encrypt        = true
-  }
-}
--- environments/dev/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags = {
-    project     = var.project_name
-    environment = var.environment
-    ManagedBy   = "Terraform"
-    Owner       = "Pranay"
-  }
-}
