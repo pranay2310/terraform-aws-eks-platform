5aa5b83721fdba7d231b8e6430495a34722a7f33
Pranaysagar <pranaysagarbhelave@gmail.com>
2026-08-04 14:21:46 +0530

    gg

--- platform/modules/aws-load-balancer-controller/main.tf
+++ platform/modules/aws-load-balancer-controller/main.tf
@@ -3,8 +3,14 @@ resource "helm_release" "this" {
   name             = "aws-load-balancer-controller"
   repository       = "https://aws.github.io/eks-charts"
   chart            = "aws-load-balancer-controller"
+
   namespace        = "kube-system"
   create_namespace = false
+
+  atomic          = true
+  cleanup_on_fail = true
+  wait            = true
+  timeout         = 600
 
   values = [
     file(var.values_file)
